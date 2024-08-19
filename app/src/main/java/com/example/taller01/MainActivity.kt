8f23649d808ca06fdbb48bf77d6dee7850af404c
Santiago Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-08-19 15:18:28 -0500

    Navegación entre pantalla principal y explorar destinos

--- app/src/main/java/com/example/taller01/MainActivity.kt
+++ app/src/main/java/com/example/taller01/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.taller01
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var botonExplorar = findViewById<Button>(R.id.btnExplorar)
+        var botonFav = findViewById<Button>(R.id.btnFavoritos)
+        var botonRecomendaciones = findViewById<Button>(R.id.btnRecomendaciones)
+
+        botonExplorar.setOnClickListener {
+            val intentPantalla2 = Intent(this, Pantalla2::class.java)
+            startActivity(intentPantalla2)
+        }
     }
 }
