d3f3de903b5ed9db25ffcbfaefc4e9df5346e2c6
monts <nmontanoa@javeriana.edu.co>
2024-08-24 00:55:02 -0500

    General

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.gson)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
 }
